e5415ac2c625705bee008f9d63381859553e93a0
AlefSRGS <alefsrodrigues03@gmail.com>
2023-10-15 18:00:52 -0300

    classe arena atualizada

--- src/Arena.java
+++ src/Arena.java
@@ -4,41 +4,68 @@ public class Arena {
     private Field leftField;
     private Field midField;
     private Field rightField;
+    private int currentRound;
+    private int finalRound;
     private boolean gameOver;
 
     
     public Arena(User[] lobbyUsers1, User[] lobbyUsers2) {
         this.player1 = lobbyUsers1[0];
         this.player2 = lobbyUsers2[0];
-        this.fieldPlayer1 = new Field();
-        this.fieldPlayer2 = new Field();
+        this.leftField = new Field();
+        this.midField = new Field();
+        this.rightField = new Field();
+        this.currentRound = 1;
+        this.finalRound = 7;
         this.gameOver = false;
+    }
+
+    public void setFinalRound(int finalRound) {
+        this.finalRound = finalRound;
     }
 
     // Método para realizar um turno de jogo
     public void playTurn() {
-        // Lógica para um turno de jogo (colocar cartas no campo, atacar, etc.)
-        // Atualizar os campos de cartas e pontos de vida conforme necessário
-
-        // Exemplo simples: decrementar 1 ponto de vida de ambos os jogadores a cada turno
-        player1Life--;
-        player2Life--;
-
+        
         // Verificar se o jogo terminou
-        if (player1Life <= 0 || player2Life <= 0) {
+        if (currentRound > finalRound) {
             gameOver = true;
             endGame();
         }
     }
 
+    private int decideSituationField(Field fieldToSetSituation){
+        if(fieldToSetSituation.getYourSideFieldTotalPower() > fieldToSetSituation.getOpponentSideFieldTotalPower()){
+            return 1;
+        }
+        else if(fieldToSetSituation.getYourSideFieldTotalPower() < fieldToSetSituation.getOpponentSideFieldTotalPower()){
+            return 2;
+        }
+        return 0;
+    }
     // Método para finalizar o jogo
     private void endGame() {
-        if (player1Life <= 0) {
-            System.out.println(player1.getUsername() + " venceu!");
-        } else if (player2Life <= 0) {
-            System.out.println(player2.getUsername() + " venceu!");
+        int situationLeftField = decideSituationField(leftField);
+        int situationMidField = decideSituationField(midField);
+        int situationRightField = decideSituationField(rightField);
+        int[] situationGame = {situationLeftField, situationMidField, situationRightField};
+        int countLocationPlayer1Win = 0;
+        int countLocationPlayer2Win = 0;
+        for (int i = 0; i < situationGame.length; i++) {
+            if(situationGame[i] == 1){
+                countLocationPlayer1Win++;
+            }
+            if(situationGame[i] == 2){
+                countLocationPlayer2Win++;
+            }
         }
-        // Lógica para conceder recompensas, registrar estatísticas, etc.
+        if (countLocationPlayer1Win > countLocationPlayer2Win) {
+            System.out.println(player1.getUsername() + " venceu!");
+        } else if (countLocationPlayer1Win < countLocationPlayer2Win) {
+            System.out.println(player2.getUsername() + " venceu!");
+        }else{
+            System.out.println("Empate");
+        }
     }
 
     // Método para iniciar o jogo
