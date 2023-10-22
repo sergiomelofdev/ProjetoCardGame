27e646ab569e0ac3728b4ba0015ecbfd6546ea50
AlefSRGS <alefsrodrigues03@gmail.com>
2023-10-21 22:51:08 -0300

    novos metodos das classe arena criados

--- src/Arena.java
+++ src/Arena.java
@@ -1,27 +1,32 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Arena {
-    private ArrayList<Field> fields; //todos os campos da arena
-    private User player1; //jogador 1
-    private User player2; //jogador 2
-    private Card[] choseDeckplayer1; //deck do jogador 1
-    private Card[] choseDeckplayer2; //deck do jogador 2
-    private ArrayList<Card> player1Hand; //mao atual do jogador 1
-    private ArrayList<Card> player2Hand; //mao atual do jogador 2
-    private int temporalEnergyplayer1; //número de energia de cada jogador
-    private int temporalEnergyplayer2; //número de energia de cada jogador
-    private ArrayList<Card> garbage; //lugar onde as cartas descartadas vao
-    private ArrayList<Card> cemetery; //lugar onde as cartas destruidas vao
-    private int currentRound; //número da rodada atual
-    private int finalRound; //número da rodada final
-    private boolean gameOver; //se o jogo acabou
+    protected ArrayList<Field> fields; //todos os campos da arena
+    protected User player1; //jogador 1
+    protected User player2; //jogador 2
+    protected Card[] choseDeckplayer1; //deck do jogador 1
+    protected int qtdCardInDeckPlayer1;
+    protected Card[] choseDeckplayer2; //deck do jogador 2
+    protected int qtdCardInDeckPlayer2;
+    protected ArrayList<Card> player1Hand; //mao atual do jogador 1
+    protected ArrayList<Card> player2Hand; //mao atual do jogador 2
+    protected int temporalEnergyplayer1; //número de energia de cada jogador
+    protected int temporalEnergyplayer2; //número de energia de cada jogador
+    protected ArrayList<Card> garbage; //lugar onde as cartas descartadas vao
+    protected ArrayList<Card> cemetery; //lugar onde as cartas destruidas vao
+    protected int currentRound; //número da rodada atual
+    protected int finalRound; //número da rodada final
+    protected boolean gameOver; //se o jogo acabou
 
     
     public Arena(Lobby lobbyTeamBlue, Lobby lobbyTeamRed) {
         this.player1 = lobbyTeamBlue.getPlayers()[0];
         this.player2 = lobbyTeamRed.getPlayers()[0];
-        //this.choseDeckplayer1 = player1.getChoseDeck();
-        //this.choseDeckplayer2 = player2.getChoseDeck();
+        this.choseDeckplayer1 = player1.getChoseDeck().clone();
+        this.choseDeckplayer2 = player2.getChoseDeck().clone();
+        this.qtdCardInDeckPlayer1 = choseDeckplayer1.length;
+        this.qtdCardInDeckPlayer2 = choseDeckplayer2.length;
         this.garbage = new ArrayList<Card>();
         this.cemetery = new ArrayList<Card>();
         Field field1 = new Field();
@@ -30,7 +35,6 @@ public class Arena {
         fields.add(field1);
         fields.add(field2);
         fields.add(field3);
-        this.currentRound = 0;
         this.finalRound = 7;
         this.gameOver = false;
     }
@@ -39,22 +43,75 @@ public class Arena {
         this.finalRound = finalRound;
     }
 
-    public void buyCard(int qtdCardToBuy) {
+    public Card removeCardInDeck(int posCardInDeck, User player){
+        if(player.compareTo(player1)){
+            for (int i = posCardInDeck; i < choseDeckplayer1.length; i++) {
+                if(choseDeckplayer1[i] == null){
+                    break;
+                }
+                if(i == choseDeckplayer1.length-1 && choseDeckplayer1 != null){
+                    choseDeckplayer1[i] = null;
+                    break;
+                }
+                choseDeckplayer1[i] = choseDeckplayer1[i+1];
+            }
+            qtdCardInDeckPlayer1--;
+            return choseDeckplayer1[posCardInDeck];
+        }else{
+            for (int i = posCardInDeck; i < choseDeckplayer2.length; i++) {
+                if(choseDeckplayer2[i] == null){
+                    break;
+                }
+                if(i == choseDeckplayer2.length-1 && choseDeckplayer2[i] != null){
+                    choseDeckplayer2[i] = null;
+                    break;
+                }
+                choseDeckplayer2[i] = choseDeckplayer2[i+1];
+            }
+            qtdCardInDeckPlayer2--;
+            return choseDeckplayer2[posCardInDeck];
+        }
+    }
+    public void buyCard(int qtdCardToBuy, User player) {
         Random random = new Random();
+        if(player.compareTo(player1)){
+            for (int i = 0; i < qtdCardToBuy; i++) {
+                int posCardInDeck = random.nextInt(qtdCardInDeckPlayer1);
+                player1Hand.add(removeCardInDeck(posCardInDeck, player1));
+            }
+        }else{
+            for (int i = 0; i < qtdCardToBuy; i++) {
+                int posCardInDeck = random.nextInt(qtdCardInDeckPlayer2);
+                player2Hand.add(removeCardInDeck(posCardInDeck, player2));
+            }
+        }
         
+    }
+    protected void beginTurn(){
+        buyCard(3, player1);
+        buyCard(3, player2);
+        Random random = new Random();
+        int coin = random.nextInt(2);
+        if(coin == 0){
+            //player 1 começa
+        }else{
+            //player 2 começa
+        }
     }
     // Método para realizar um turno de jogo
     public void playTurn() {
+        currentRound++;
         // Verificar se o jogo terminou
         if (currentRound > finalRound) {
             gameOver = true;
             endGame();
+            return;
         }
-        currentRound++;
-        //inicio da partida
-        if(currentRound == 1){
-
-        }
+        //inicio do turno
+        temporalEnergyplayer1++;
+        temporalEnergyplayer2++;
+        buyCard(1, player1);
+        buyCard(1, player2);
         
     }
 
@@ -68,9 +125,9 @@ public class Arena {
         return 0;
     }
     // Método para finalizar o jogo
-    private void endGame() {
+    protected void endGame() {
         int countLocationplayer1Wins = 0;
-        int countLocationplayer2Wins = 0; 
+        int countLocationplayer2Wins = 0;
         //contando em quantos campos cada jogador venceu
         for(int i = 0; i < fields.size(); i++) {
             if(decideSituationField(fields.get(i)) == 1){
@@ -93,7 +150,7 @@ public class Arena {
     public void startGame() {
         System.out.print("Iniciando partida entre ");
         System.out.println(player1.getUsername() + " e " + player2.getUsername());
-        
+        beginTurn();
         // Lógica para inicialização do jogo, como distribuir cartas, escolher quem começa, etc.
         while (!gameOver) {
             playTurn();
--- src/ArenaDupla.java
+++ src/ArenaDupla.java
@@ -1,4 +1,110 @@
+import java.util.Random;
 
 public class ArenaDupla  extends Arena {
-    
+    private User player3;
+    private User player4;
+    private Card[] choseDeckplayer3;
+    private Card[] choseDeckplayer4;
+    private int qtdCardInDeckPlayer3;
+    private int qtdCardInDeckPlayer4;
+    private int temporalEnergyplayer3;
+    private int temporalEnergyplayer4;
+
+    public ArenaDupla(Lobby lobbyTeamBlue, Lobby lobbyTeamRed){
+        super(lobbyTeamBlue, lobbyTeamRed);
+        this.player3 = lobbyTeamBlue.getPlayers()[1];
+        this.player4 = lobbyTeamRed.getPlayers()[1];
+        this.choseDeckplayer3 = player3.getChoseDeck().clone();
+        this.choseDeckplayer4 = player4.getChoseDeck().clone();
+        this.qtdCardInDeckPlayer3 = choseDeckplayer3.length;
+        this.qtdCardInDeckPlayer4 = choseDeckplayer4.length;
+        Field field4 = new Field();
+        Field field5 = new Field();
+        fields.add(field4);
+        fields.add(field5);
+    }
+
+    @Override
+    protected void beginTurn(){
+        buyCard(3, player1);
+        buyCard(3, player2);
+        buyCard(3, player3);
+        buyCard(3, player4);
+        Random random = new Random();
+        int coin = random.nextInt(2);
+        if(coin == 0){
+            //time 1 começa
+        }else{
+            //time 2 começa
+        }
+    }
+
+    @Override
+    public void playTurn() {
+        currentRound++;
+        // Verificar se o jogo terminou
+        if (currentRound > finalRound) {
+            gameOver = true;
+            endGame();
+            return;
+        }
+        //inicio do turno
+        temporalEnergyplayer1++;
+        temporalEnergyplayer2++;
+        temporalEnergyplayer3++;
+        temporalEnergyplayer4++;
+        buyCard(1, player1);
+        buyCard(1, player2);
+        buyCard(1, player3);
+        buyCard(1, player4);
+        
+    }
+
+    @Override
+    public void buyCard(int qtdCardToBuy, User player) {
+        Random random = new Random();
+        if(player.compareTo(player1)){
+            for (int i = 0; i < qtdCardToBuy; i++) {
+                int posCardInDeck = random.nextInt(qtdCardInDeckPlayer1);
+                player1Hand.add(removeCardInDeck(posCardInDeck, player1));
+            }
+        }else{
+            for (int i = 0; i < qtdCardToBuy; i++) {
+                int posCardInDeck = random.nextInt(qtdCardInDeckPlayer2);
+                player2Hand.add(removeCardInDeck(posCardInDeck, player2));
+            }
+        }
+        
+    }
+
+    @Override
+    public Card removeCardInDeck(int posCardInDeck, User player){
+        if(player.compareTo(player1)){
+            for (int i = posCardInDeck; i < choseDeckplayer1.length; i++) {
+                if(choseDeckplayer1[i] == null){
+                    break;
+                }
+                if(i == choseDeckplayer1.length-1 && choseDeckplayer1 != null){
+                    choseDeckplayer1[i] = null;
+                    break;
+                }
+                choseDeckplayer1[i] = choseDeckplayer1[i+1];
+            }
+            qtdCardInDeckPlayer1--;
+            return choseDeckplayer1[posCardInDeck];
+        }else if(player.compareTo(player2)){
+            for (int i = posCardInDeck; i < choseDeckplayer2.length; i++) {
+                if(choseDeckplayer2[i] == null){
+                    break;
+                }
+                if(i == choseDeckplayer2.length-1 && choseDeckplayer2[i] != null){
+                    choseDeckplayer2[i] = null;
+                    break;
+                }
+                choseDeckplayer2[i] = choseDeckplayer2[i+1];
+            }
+            qtdCardInDeckPlayer2--;
+            return choseDeckplayer2[posCardInDeck];
+        }
+    }
 }
