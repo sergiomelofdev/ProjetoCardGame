ff246f07632f0db724397662e45dc4f6fa6b502a
Álef Silva Rodrigues <79549354+AlefSRGS@users.noreply.github.com>
2023-10-30 17:46:48 +0000

    adicionado o novo booster pack

--- src/ClientService/Store.java
+++ src/ClientService/Store.java
@@ -1,5 +1,8 @@
 import java.util.Random;
 
+import Cards.Card;
+import Enumerates.EnumRarityCard;
+
 public class Store {
     private String cardNumber;
     private String verifierCode;
@@ -25,22 +28,34 @@ public class Store {
         }
     }
 
+    public void buyBoosterPremium(User user, Inventory inventory) {
+        int boosterPrice = 200; // Preço de um booster em CardCoins (ajuste conforme necessário)
+
+        if (inventory.wasteCardCoin(boosterPrice)) {
+            Card boosterPack = generateBoosterPremium();
+            
+            if(!inventory.incrementCardInInventory(card)){ //se o usuario n conseguir adicionar a carta no inventario, 
+                inventory.earnCardCoin(200);      //quer dizer q ele ja tem ela logo ganha um "reembolso" de cardCoins
+            }
+        }
+    }
+
+
     // Método para gerar um pacote de booster com cartas aleatórias
     private Card[] generateBoosterPackRandomCards(int qtdCardsBoosterPack) {
         Card[] boosterPack = new Card[qtdCardsBoosterPack];
         //para preencher o booster pack é gerado 12 cartas aleatorias
-        Random random = new Random();
         for (int posBoosterPack = 0; posBoosterPack < qtdCardsBoosterPack; posBoosterPack++) {
-            Card card = new Card("Carta Aleatória " + (posBoosterPack + 1), "imagem","type", getRandomRarity(random), 2, 3, 5, "ability");
+            Card card = new Card("Carta Aleatória " + (posBoosterPack + 1), "imagem","type", card.generateRarityCard(), 2, 3, 5, "ability");
             boosterPack[posBoosterPack] = card;
             posBoosterPack++;
         }
         return boosterPack;
     }
-
-    // Método auxiliar para obter uma raridade aleatória
-    private Card.Rarity getRandomRarity(Random random) {
-        int index = random.nextInt(Card.Rarity.values().length);
-        return Card.Rarity.values()[index];
+    private Card generateBoosterPremium() {
+        //booster q gera apenas uma carta aleatória mas ela sempre sera lendaria
+        Card card = new Card("Carta Aleatória", "imagem","type", EnumRarityCard.LEGENDARY, 2, 3, 5, "ability");
+        return card;
     }
+
 }
